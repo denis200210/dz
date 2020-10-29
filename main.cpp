9851fead1fe95796c088c8ddf5ae77b5fae51275
denis200210 <71909269+denis200210@users.noreply.github.com>
2020-10-29 15:59:14 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
 
-void aim(int& x, int& y) 
+void aim(int& x, int& y)
 {
     std::cout << "введите координаты х :";
     std::cin >> x;
     std::cout << "введите координаты у: ";
     std::cin >> y;
 }
-void randd(int& x, int& y) 
+void randd(int& x, int& y)
 {
     // добавляет к введенному значении рандомное число чтобы было сложнее попасть
-    
-    x += rand ()%3;
-    y += rand() %3;
+
+    x += rand() % 3;
+    y += rand() % 3;
 }
 int getranddPoints() // мишень  и выстрелы в нее
 {
-    int x, y, ball;
+    int x, y, ball=0;
     double R;
 
     aim(x, y);
     randd(x, y);
     R = sqrt(x * x + y * y);
-    ball = (R <= 5) ? abs(static_cast<int>(ceil(R)) - 5) : 0;
+    if (R > 5) {
+    }
+    else if (R > 4) {
+        ball += 1;
+    }
+    else if (R > 3) {
+        ball += 2;
+    }
+    else if (R > 2) {
+        ball += 3;
+    }
+    else if (R > 1) {
+        ball += 4;
+    }
+    else ball += 5;
     std::cout << "You hit the point (" << x << ", " << y << ") and got " << ball << " points" << std::endl;
     return ball;
 }
@@ -35,10 +49,10 @@ int main() // посчет баллов и вывод результата
         ball += getranddPoints();
     }
     if (ball < 10) {
-    std::cout << "Вы набрали " << ball << " баллов и вы проиграли";
+        std::cout << "Вы набрали " << ball << " баллов и вы проиграли";
     }
     else {
-    std::cout << "Вы набрали " << ball << " баллов и вы выйиграли";
+        std::cout << "Вы набрали " << ball << " баллов и вы выйиграли";
     }
     return 0;
 }
